03f322cc3f62b7d99b2b9185f8e84a8d924ebc3d
Gogo602 <gogomarosi61@gmail.com>
2025-05-13 23:02:31 +0300

    File: component/roomCard; handle image rendering from appwrite

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,17 @@ const nextConfig = {
     experimental: {
       serverActions: {
         bodySizeLimit: '5mb', // You can change '5mb' to your desired limit (e.g., '10mb', '50mb')
-      },
-    },
+    }, 
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+        pathname: '/v1/storage/buckets/**',
+      }
+    ]
+  }
   };
   
   export default nextConfig;
